d4b7c717cd0664ac37922182163a627050fba985
kaneki29 <lionelguedessidieu@gmail.com>
2022-09-10 02:36:04 -0700

    getting started

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.j>
+int main()
+	{
+	 printf("size of a char:%d\n", sizeof(char));
+	 printf("size of a int:%d\n",sizeof(int));
+	 printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+ 	 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+ 	 printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+
+
+	}
